e103d8e218a21f1d317120291a31e32fea996b55
IndipendenteTrockista <progettoaniene2a@gmail.com>
2022-04-23 00:49:50 +0200

    MAPPA INTERATTIVA
    Addattiva allo schermo, centrale e con link interattivi in posizione

---
+++ res/map.js
@@ -0,0 +1,32 @@
+function setAndFindG() {
+	image = document.getElementById("intMapImg");
+	scrWidth = window.screen.availWidth;
+	
+	image.width = scrWidth * 0.9;
+	imgWidth = image.width;
+	oriWidth = 1833;
+	
+	dif = imgWidth / oriWidth;
+	
+	return dif
+};
+
+function loadMap() {
+	let g = setAndFindG();
+	console.log(g);
+	
+	map = document.getElementById("intMap");
+	//areas = document.getElementsByClassName("areas");
+	
+	areas = [
+		[ "rect", [870 * g, 380 * g, 990 * g, 450 * g], "../index.html" ]
+	]
+	
+	for ( let i = 0; i < areas.length; i++ ) {
+		let area = document.createElement("area");
+		area.shape = areas[i][0];
+		area.coords = areas[i][1];
+		area.href = areas[i][2];
+		map.append( area );
+	}
+}
