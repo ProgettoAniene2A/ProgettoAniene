90cfec4dde15dc8b149af3c4c490e75552909826
IndipendenteTrockista <progettoaniene2a@gmail.com>
2022-05-11 18:58:49 +0200

    Sistemazione mobile (n3)

--- res/script.js
+++ res/script.js
@@ -2,7 +2,7 @@ function move( url ) {
 	window.location.href = url;
 }
 
-function redirect() {
+function redirectQR() {
 	ratio = screen.width / screen.height;
 	if ( ratio < 1 ) {
 		window.location.href = "https://progettoaniene2a.github.io/ProgettoAniene/progetti-mobile/progetti.html";
